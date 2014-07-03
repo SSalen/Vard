45e7def10cb1ea673a6df791ac126e42e9ef78f2
unknown <stian.salen@STX-1671P.akeryards.as>
2014-07-03 15:19:53 +0200

    added AR app first time

---
+++ AugmentedReality/src/com/ex/augmentedreality/dbClass.java
@@ -0,0 +1,29 @@
+package com.ex.augmentedreality;
+
+import android.content.Context;
+
+public class dbClass {
+
+	
+	
+	public dbClass(Context applicationContext) {
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	public void open() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
